de4b8013811f53a2d83117d109e988af1dd6c844
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2023-04-05 10:37:51 -0700

    Updating MLflow class that simplifies interaction with MLflow.
    - Adding doc strings.
    - Fixing bug related to use of older API (`list_experiments`) that does not exist in new versions. Changed to `search_experiments`.
    - Fixing bug related to page_token not being used when invoking mlflow's `search_run` function.

--- training/xtime/contrib/mlflow_ext.py
+++ training/xtime/contrib/mlflow_ext.py
@@ -37,6 +37,14 @@ class MLflow(object):
     def set_tags(
         dataset: t.Optional[str] = None, run_type: t.Optional[RunType] = None, task: t.Optional[Task] = None, **kwargs
     ) -> None:
+        """Helper method that sets tags for active MLflow run.
+
+        Args:
+            dataset: Dataset name and version in format "name[:version]".
+            run_type: Type of this MLflow run (`train`, `hpo` etc.).
+            task: Task being solved in this run.
+            kwargs: dictionary of additional tags to set.
+        """
         if dataset:
             dataset_name, dataset_version = parse_dataset_name(dataset)
             mlflow.set_tags({"dataset_name": dataset_name, "dataset_version": dataset_version})
@@ -48,13 +56,21 @@ class MLflow(object):
 
     @staticmethod
     def get_experiment_ids(client: t.Optional[MlflowClient] = None) -> t.List[str]:
+        """Return all MLflow experiment IDs.
+
+        Args:
+           client: MLflow client to use. If not provided, a default client will be used.
+
+        Returns:
+             List of IDs of experiments recorded in MLflow server. Empty list means there's no experiments.
+        """
         if client is None:
             client = MlflowClient()
 
         ids: t.List[str] = []
         page_token: t.Optional[str] = None
         while True:
-            experiments: PagedList[Experiment] = client.list_experiments(page_token=page_token)
+            experiments: PagedList[Experiment] = client.search_experiments(page_token=page_token)
             ids.extend(e.experiment_id for e in experiments)
             page_token = experiments.token
             if not page_token:
@@ -68,12 +84,26 @@ class MLflow(object):
         experiment_ids: t.Optional[t.List[str]] = None,
         filter_string: t.Optional[str] = None,
     ) -> t.List[Run]:
+        """Return MLflow runs that match given query (filter string).
+
+        Args:
+            client: MLflow client to use. If not provided, a default client will be used.
+            experiment_ids: List of IDs of experiments within which runs will be searched.
+            filter_string: Search query in format accepted by MLflow API. Consult MLflow API documentation on exact
+                format of this string.
+
+        Returns:
+            List of found MLflow runs.
+        """
         if client is None:
             client = MlflowClient()
 
         runs: t.List[Run] = []
+        page_token: t.Optional[str] = None
         while True:
-            _runs: PagedList[Run] = client.search_runs(experiment_ids=experiment_ids, filter_string=filter_string)
+            _runs: PagedList[Run] = client.search_runs(
+                experiment_ids=experiment_ids, filter_string=filter_string, page_token=page_token
+            )
             runs.extend(_runs)
             page_token = _runs.token
             if not page_token:
@@ -83,6 +113,11 @@ class MLflow(object):
 
     @staticmethod
     def create_experiment(client: t.Optional[MlflowClient] = None) -> None:
+        """Create a new MLflow experiment with name specified in `MLFLOW_EXPERIMENT_NAME` environment variable.
+
+        Args:
+            client: MLflow client to use. If not provided, a default client will be used.
+        """
         if client is None:
             client = MlflowClient()
 
@@ -94,10 +129,27 @@ class MLflow(object):
 
     @staticmethod
     def get_tags_from_env() -> t.Dict:
+        """Return dictionary of tags specified via environment.
+
+        Users can provide addition tags to be associated with MLflow runs. It's the same format that hyperparameters are
+            used when defined in strings: `export MLFLOW_TAGS="params:tag1=value1;tag2=value2"`.
+
+        Returns:
+            Dictionary or tags or empty dictionary.
+        """
         return hparams.from_string(os.environ.get("MLFLOW_TAGS", None))
 
     @staticmethod
     def get_artifact_path(run: t.Optional[Run] = None, ensure_exists: bool = True) -> Path:
+        """Return path to artifact directory for given MLflow run.
+
+        Args:
+            run: MLflow run or none. If none, currently active MLflow run will be used.
+            ensure_exists: If true and path does not exist, it will be created.
+
+        Returns:
+            Path to artifact directory (that may or may not exist depending on this run and method parameters).
+        """
         if run is not None:
             artifact_uri = run.info.artifact_uri
         elif mlflow.active_run() is not None:
@@ -110,5 +162,13 @@ class MLflow(object):
         return local_dir
 
     @staticmethod
-    def init_run(run: t.Optional[Run]):
+    def init_run(run: t.Optional[Run]) -> None:
+        """Initialize MLflow run.
+
+        For now, it does not do a lot of things, mainly ensuring that the artifact directory exists. So, it's a wrapper
+        over MLflow.get_artifact_path method to better communicate the usage scenarios of this method.
+
+        Args:
+            run: MLflow run to initialize. If none, currently active run will be used.
+        """
         _ = MLflow.get_artifact_path(run, ensure_exists=True)
