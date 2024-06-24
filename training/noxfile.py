713d9ca03645fa47a2c0da3f854192e39167971c
Sergey Serebryakov <serebryakov.sergey@gmail.com>
2024-06-24 16:33:37 +0000

    Adding multiple options for tests - with pinned and latest secondary dependencies.

--- training/noxfile.py
+++ training/noxfile.py
@@ -14,26 +14,6 @@
 # limitations under the License.
 ###
 
-"""
-References:
-- Why I Like Nox / https://hynek.me/articles/why-i-like-nox
-
-
-Notes
-- Anaconda setup.
-  - Install nox in base environment: `conda install nox`
-  - Create multiple python environments`conda create -n 3.9 python=3.9; conda create -n 3.11 python=3.11`.
-    It seems like there's no need to create an environment for the same version as base environment (3.10 in this
-    example).
-  - Note paths to these environments: `conda env list`
-  - Run nox in BASE environment: `PATH="${CONDA_PREFIX}/envs/3.9/bin:${CONDA_PREFIX}/envs/3.11/bin:${PATH}" nox`
-- The script itself runs in python environment that was used to launch the script. It's the `session` object that
-  communicates with the actual runtime environment.
-- The `posargs` is a list of strings e.g. ["--name=val"]
-- The `pip` can install poetry projects (pyproject.toml) starting 19.0 version.
-- Output of `run` and `install` can be sent to a file (https://github.com/wntrblm/nox/issues/218).
-"""
-
 import os
 import typing as t
 from pathlib import Path
@@ -50,29 +30,39 @@ if "XTIME_NOX_PYTHON_VERSIONS" in os.environ:
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 
 
-def _print_header(session: nox.Session) -> None:
-    pyver: str = session.run((Path(session.bin) / "python").as_posix(), "--version", silent=True)
-    nox_info = f"(location='{session.virtualenv.location}', python='{session.python}', version='{pyver}')"
-    rt_info = f"(cwd='{Path.cwd()}', posargs={session.posargs})"
-    print(f"nox={nox_info}, runtime={rt_info}")
-
-
 @nox.session(python=XTIME_NOX_PYTHON_VERSIONS, name="unit")
-def unit_tests(session: nox.Session) -> None:
-    """Run XTIME unit tests.
+@nox.parametrize("deps", ["pinned", "latest"])
+def unit_tests(session: nox.Session, deps: str) -> None:
+    """Run XTIME training unit tests.
 
     The `posargs` are passed through to pytest (e.g., -m datasets). Current working directory - location of the
     `noxfile.py`.
+
+    Args:
+        session: Current nox session.
+        deps: When pinned, all dependencies are fixed to those specified in `poetry.lock`. When latest, only primary
+            dependencies specified in the pyproject.toml may be pinned. All other, secondary dependencies, may or may
+            not be pinned depending on metadata of primary dependencies. When pinned dependencies are used, the poetry
+            must be available externally.
     """
-    _print_header(session)
+    # Install this project and pytest (with `pip install`).
+    install_args: t.List[str] = [".[all]", "pytest"]
 
-    # Not really required, but let's use pytest version specified in project's dev dependencies.
-    proj_cfg: t.Dict = nox.project.load_toml("pyproject.toml")
-    dev_deps: t.Dict = proj_cfg["tool"]["poetry"]["group"]["dev"]["dependencies"]
-    session.install(f"pytest=={dev_deps['pytest']}")
+    # If pinned deps to be used, export constraints from `poetry.lock` file using `poetry`.
+    if deps == "pinned":
+        constraints_file: str = (Path(session.create_tmp()).resolve() / "constraints.txt").as_posix()
+        session.run(
+            "poetry",
+            "export",
+            "--format=constraints.txt",
+            f"--output={constraints_file}",
+            "--without-hashes",
+            "--with=dev",
+            "--without=eda",
+            "--extras=all",
+            external=True,
+        )
+        install_args.append(f"--constraint={constraints_file}")
 
-    # Install current project (only `main` dependencies are installed).
-    session.install(".[all]")
-
-    # Run unit tests.
+    session.install(*install_args)
     session.run("pytest", "-v", *session.posargs)
